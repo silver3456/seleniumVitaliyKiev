7372c6ab3bd8c4c39356bad29e7849a80cf8dd0e
silver3456 <avseliverov@gmail.com>
2019-02-17 13:54:32 -0800

    Added TestRunner

--- src/test/java/com/usa/web/TestRunner.java
+++ src/test/java/com/usa/web/TestRunner.java
@@ -1,13 +1,11 @@
 package com.usa.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
+
 
 public class TestRunner {
     protected WebDriver driver;
